c1bd10d832524c7d92703f6b8e56a44f3b8e6bce
Jatin Sharma <87287391+MrtitaniumJ@users.noreply.github.com>
2022-12-11 21:21:02 +0530

    Create Array Pair Sum Divisibility Problem.java

---
+++ December/11 Dec/Array Pair Sum Divisibility Problem.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String s1 = br.readLine().trim();
+            String[] S1 = s1.split(" ");
+            int n = Integer.parseInt(S1[0]);
+            int k = Integer.parseInt(S1[1]);
+            String s2 = br.readLine().trim();
+            String[] S2 = s2.split(" ");
+            int[] nums = new int[n];
+            for (int i = 0; i < n; i++) {
+                nums[i] = Integer.parseInt(S2[i]);
+            }
+            Solution ob = new Solution();
+            boolean ans = ob.canPair(nums, k);
+            if (ans)
+                System.out.println("True");
+            else
+                System.out.println("False");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public boolean canPair(int[] nums, int k) {
+        // Code here
+        if(nums.length % 2 == 1)
+            return false;
+        
+        int[] total = new int[100001];
+        for(int num1 : nums) {
+            int num2 = num1 % k;
+            if(total[(k - num2) % k] != 0) total[(k - num2) % k]--;
+            else total[num2]++;
+        }
+        for(int t : total) {
+            if(t > 0)
+                return false;
+        }
+        return true;
+    }
+}
